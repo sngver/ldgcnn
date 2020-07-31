2b44c234cd589ecf9ffe9aa18938cac4e2b6ae04
sngver <67322452+sngver@users.noreply.github.com>
2020-07-31 15:06:25 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -59,7 +59,8 @@ HOSTNAME = socket.gethostname()
 # ModelNet40 official train/test split
 TRAIN_FILES = provider.getDataFiles( \
     os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/train_files.txt'))
-TEST_FILES = os.path.join(BASE_DIR, 'data/mydataset_hdf5/test_files.txt'))
+TEST_FILES = provider.getDataFiles( \
+    os.path.join(BASE_DIR, 'data/mydataset_hdf5/test_files.txt'))
 
 is_training = False
 #%%
